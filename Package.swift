9abb8e9164d93b37f59ad429311b88cf9fdd44b1
Bernie.Du <553296416@qq.com>
2025-06-20 14:23:50 +0800

    Build 1.11.0-OpenSSL-1-1-1u

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "CSSH",
-                      url: "https://github.com/DimaRU/Libssh2Prebuild/releases/download/1.11.0-OpenSSL-1-1-1w/CSSH-1.11.0-OpenSSL-1-1-1w.xcframework.zip",
-                      checksum: "f2a14236019b617019f522001c89ad5db2125e8f350cbcde5ed6e96f50842157")
+                      url: "https://github.com/d553296416/Libssh2Prebuild/releases/download/1.11.0-OpenSSL-1-1-1u/CSSH-1.11.0-OpenSSL-1-1-1u.xcframework.zip",
+                      checksum: "b08b3132b57c7c18825fd1b56dc987ea9b9d803f80e9cdc484d6a0962bd1af61")
     ]
 )
